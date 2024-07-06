4549d656f06de0d0172f21929c5a4f213fe4a750
MESONICDevaj <147987753+mesonicDEVAJ18@users.noreply.github.com>
2024-07-06 22:43:06 +0400

    Version_3.0

--- civilianside/app.py
+++ civilianside/app.py
@@ -6,12 +6,20 @@ import os
 # Initialize Flask application
 app = Flask(__name__)
 
-# Define path to SQLite database file
-db_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'dataentry', 'location.db'))
+# Define base directory and database paths
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'dataentry'))
+location_db_file_path = os.path.join(basedir, 'location.db')
+announce_db_file_path = os.path.join(basedir, 'announce.db')
 
-# Configure SQLAlchemy to use SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_file_path}'
+# Configure SQLAlchemy to use the SQLite databases with binds
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{announce_db_file_path}'
+app.config['SQLALCHEMY_BINDS'] = {
+    'location': f'sqlite:///{location_db_file_path}'
+}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Secret key for session management 
+app.secret_key = 'supersecretkey'
 
 # Initialize SQLAlchemy object with the Flask app
 db = SQLAlchemy(app)
@@ -24,6 +32,7 @@ class LocationType(Enum):
 
 # Define SQLAlchemy Model for locations
 class Location(db.Model):
+    __bind_key__ = 'location'  # Bind this model to the 'location' database
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     details = db.Column(db.String(500), nullable=False)
@@ -34,15 +43,28 @@ class Location(db.Model):
     def __repr__(self) -> str:
         return f"{self.name}"
 
+# Model for announcements stored in the main database
+class Announce(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    heading = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"{self.heading}"
+
 # Route for the index page
 @app.route("/")
 def index():
-    return render_template("index.html")
+    entries = Announce.query.all()[::-1]
+    return render_template("index.html", entries = entries)
 
 # Route for the map page
 @app.route("/map")
 def show_map():
-    return render_template("map.html")
+    danger  = Location.query.filter_by(location_type='DANGER').all()
+    safe = Location.query.filter_by(location_type='SAFE').all()
+    resource = Location.query.filter_by(location_type='RESOURCE').all()
+    return render_template("map.html", danger = danger, safe = safe, resource = resource)
 
 # Route for displaying resources (locations with location_type='RESOURCE')
 @app.route("/resources")
